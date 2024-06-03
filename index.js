3640507fb51af20d722b210161169ec9efd1d8ca
nevinmanoj <NEVINMANOJP@GMAIL.COM>
2024-06-03 21:14:06 +0530

    base commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the start")
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server listening at http://localhost:${port}`);
+});
